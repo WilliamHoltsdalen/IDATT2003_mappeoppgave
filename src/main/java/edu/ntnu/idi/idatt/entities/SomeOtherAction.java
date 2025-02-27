1d5190b37b665993910abc237bf57517f74b2fab
WilliamHoltsdalen <williamlh@hotmail.no>
2025-02-27 12:02:09 +0100

    Update perform method signature for TileAction interface

--- src/main/java/edu/ntnu/idi/idatt/entities/SomeOtherAction.java
+++ src/main/java/edu/ntnu/idi/idatt/entities/SomeOtherAction.java
@@ -2,7 +2,7 @@ package edu.ntnu.idi.idatt.entities;
 
 public class SomeOtherAction implements TileAction {
 
-  public void perform(Player player) {
+  public void perform(Player player, Board board) {
 
   }
 }
--- src/main/java/edu/ntnu/idi/idatt/entities/TileAction.java
+++ src/main/java/edu/ntnu/idi/idatt/entities/TileAction.java
@@ -2,5 +2,5 @@ package edu.ntnu.idi.idatt.entities;
 
 public interface TileAction {
 
-  public void perform(Player player);
+  public void perform(Player player, Board board);
 }
