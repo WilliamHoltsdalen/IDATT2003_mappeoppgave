2bcd7400977e58fa459fde42b1546e6c7e59feb8
WilliamHoltsdalen <williamlh@hotmail.no>
2025-03-25 16:20:55 +0100

    refactor: Move tile actions to ladderBoard.json file

--- src/main/java/edu/ntnu/idi/idatt/model/BoardGame.java
+++ src/main/java/edu/ntnu/idi/idatt/model/BoardGame.java
@@ -6,9 +6,7 @@ import static edu.ntnu.idi.idatt.model.validators.ArgumentValidator.boardGameCre
 import static edu.ntnu.idi.idatt.model.validators.ArgumentValidator.boardGameSetCurrentPlayerValidator;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class BoardGame {
   private Board board;
@@ -22,7 +20,6 @@ public class BoardGame {
     this.roundNumber = 0;
 
     createBoard(boardRows, boardColumns);
-    addTileActions();
     addPlayers(players);
     createDice(diceCount);
   }
@@ -32,7 +29,6 @@ public class BoardGame {
     this.roundNumber = 0;
 
     this.board = board;
-    addTileActions();
     addPlayers(players);
     createDice(diceCount);
   }
@@ -93,24 +89,5 @@ public class BoardGame {
 
   public void incrementRoundNumber() {
     roundNumber++;
-  }
-
-  private void addTileActions() {
-    Map<Integer, Integer> ladderMap = new HashMap<>();
-    ladderMap.put(5, 44);
-    ladderMap.put(39, 58);
-    ladderMap.put(46, 87);
-
-    Map<Integer, Integer> slideMap = new HashMap<>();
-    slideMap.put(34, 13);
-    slideMap.put(82, 78);
-    slideMap.put(89, 68);
-
-    for (Map.Entry<Integer, Integer> entry : ladderMap.entrySet()) {
-      this.board.getTile(entry.getKey()).setLandAction(new LadderAction(entry.getValue(), "Ladder"));
-    }
-    for (Map.Entry<Integer, Integer> entry : slideMap.entrySet()) {
-      this.board.getTile(entry.getKey()).setLandAction(new LadderAction(entry.getValue(), "Slide"));
-    }
   }
 }
