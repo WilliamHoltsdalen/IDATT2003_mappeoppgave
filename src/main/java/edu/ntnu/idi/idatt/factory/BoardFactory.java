155d8470015e3641852c6f52ccbf22b548c6516f
WilliamHoltsdalen <williamlh@hotmail.no>
2025-03-25 21:36:52 +0100

    feat(factory): Make BoardFactory class with classic board

---
+++ src/main/java/edu/ntnu/idi/idatt/factory/BoardFactory.java
@@ -0,0 +1,102 @@
+package edu.ntnu.idi.idatt.factory;
+
+import edu.ntnu.idi.idatt.model.Board;
+import edu.ntnu.idi.idatt.model.Tile;
+import edu.ntnu.idi.idatt.model.LadderAction;
+import edu.ntnu.idi.idatt.utils.BoardFileHandlerGson;
+import edu.ntnu.idi.idatt.utils.interfaces.FileHandler;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BoardFactory {
+
+  /**
+   * Creates a Board object based on a predefined variant (hardcoded).
+   *
+   * @param variant A string specifying which board variant to create.
+   * @return A configured Board object.
+   * @throws IllegalArgumentException if the variant is not recognized.
+   */
+  public static Board createBoard(String variant) {
+    return switch (variant.toLowerCase()) {
+      case "classic" -> createClassicBoard();
+      case "teleporting" -> createPortalBoard();
+      default -> throw new IllegalArgumentException("Unknown board variant: " + variant);
+    };
+  }
+
+  /**
+   * Creates a Board object by reading from an external file.
+   * File handling is delegated to the BoardFileHandlerGson class.
+   *
+   * @param filePath The path to the JSON file containing board data.
+   * @return A Board object constructed from the file data.
+   * @throws IOException if an error occurs during file processing.
+   * @throws IllegalArgumentException if the file does not contain a valid board.
+   */
+  public static Board createBoardFromFile(String filePath) throws IOException {
+    FileHandler<Board> boardHandler = new BoardFileHandlerGson();
+    List<Board> boards = boardHandler.readFile(filePath);
+    if (boards.isEmpty()) {
+      throw new IllegalArgumentException("No boards found in file: " + filePath);
+    }
+    // Todo: extend to support multiple boards.
+    return boards.getFirst();
+  }
+
+  /**
+   * Creates a classic board layout.
+   *
+   * @return A classic Board object.
+   */
+  private static Board createClassicBoard() {
+    Board board = new Board();
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 10; j++) {
+        int baseId = i * 10;
+        int tileId;
+
+        // Even numbered rows (left to right)
+        if (i % 2 == 0) {
+          tileId = baseId + j + 1;
+        } else { // Odd numbered rows (right to left)
+          tileId = baseId + (10 - j);
+        }
+
+        board.addTile(new Tile(tileId, tileId + 1));
+      }
+    }
+    Map<Integer, Integer> ladderMap = new HashMap<>();
+    ladderMap.put(5, 44);
+    ladderMap.put(39, 58);
+    ladderMap.put(46, 87);
+
+    Map<Integer, Integer> slideMap = new HashMap<>();
+    slideMap.put(34, 13);
+    slideMap.put(82, 78);
+    slideMap.put(89, 68);
+
+    for (Map.Entry<Integer, Integer> entry : ladderMap.entrySet()) {
+      board.getTile(entry.getKey()).setLandAction(new LadderAction(entry.getValue(),
+          "Ladder from " + entry.getKey() + " to " + entry.getValue()));
+    }
+    for (Map.Entry<Integer, Integer> entry : slideMap.entrySet()) {
+      board.getTile(entry.getKey()).setLandAction(new LadderAction(entry.getValue(),
+          "Slide from " + entry.getKey() + " to " + entry.getValue()));
+    }
+    return board;
+  }
+
+  /**
+   * Creates an alternative board layout.
+   *
+   * @return An alternative Board object.
+   */
+  private static Board createPortalBoard() {
+    Board board = new Board();
+
+    return board;
+  }
+}
