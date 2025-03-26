239f10791f7e095ce38d965d263e2b2f0e32bdd4
WilliamHoltsdalen <williamlh@hotmail.no>
2025-03-26 20:52:49 +0100

    feat(observer): Add basic BoardGameObserver interface

---
+++ src/main/java/edu/ntnu/idi/idatt/observer/BoardGameObserver.java
@@ -0,0 +1,30 @@
+package edu.ntnu.idi.idatt.observer;
+
+import edu.ntnu.idi.idatt.model.Player;
+
+/**
+ * An interface for observers that wish to receive notifications about board game events.
+ */
+public interface BoardGameObserver {
+  /**
+   * Called when a player moves to a new tile.
+   *
+   * @param player The player who moved.
+   * @param newTileId The ID of the new tile.
+   */
+  void onPlayerMoved(Player player, int newTileId);
+
+  /**
+   * Called when the game state changes (e.g., round number increments, special events occur).
+   *
+   * @param newState A description of the new game state.
+   */
+  void onGameStateChanged(String newState);
+
+  /**
+   * Called when the game has finished.
+   *
+   * @param winner The player who won the game.
+   */
+  void onGameFinished(Player winner);
+}
