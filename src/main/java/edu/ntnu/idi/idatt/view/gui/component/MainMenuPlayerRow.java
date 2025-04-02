8cd2f745a3e662099e7663285ae107750a2c2b23
WilliamHoltsdalen <williamlh@hotmail.no>
2025-04-02 13:02:23 +0200

    feat(view): Add player color picker popup in main menu

--- src/main/java/edu/ntnu/idi/idatt/view/gui/component/MainMenuPlayerRow.java
+++ src/main/java/edu/ntnu/idi/idatt/view/gui/component/MainMenuPlayerRow.java
@@ -1,40 +1,68 @@
 package edu.ntnu.idi.idatt.view.gui.component;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+import javafx.stage.Popup;
 import org.kordamp.ikonli.javafx.FontIcon;
 
 public class MainMenuPlayerRow extends HBox {
   TextField nameTextField;
-  Circle playerCircle;
+  Button playerButton;
+  Circle playerButtonCircle;
   Button deleteButton;
+  Color defaultColor;
   boolean removable;
 
   public MainMenuPlayerRow(String defaultName, Color defaultColor, boolean removable) {
     this.defaultColor = defaultColor;
     this.removable = removable;
     this.getStyleClass().add("main-menu-player-row");
-    initialize(defaultName, color);
+    initialize(defaultName);
   }
 
-  private void initialize(String defaultName, Color color) {
-    playerCircle = new Circle(10, Color.TRANSPARENT);
-    playerCircle.setStroke(color);
-    playerCircle.setStrokeWidth(8);
-    playerCircle.getStyleClass().add("main-menu-player-circle");
+  private void initialize(String defaultName) {
+    playerButtonCircle = new Circle(10, Color.TRANSPARENT);
+    playerButtonCircle.setStroke(defaultColor);
+    playerButtonCircle.setStrokeWidth(8);
+
+    playerButton = new Button();
+    playerButton.setGraphic(playerButtonCircle);
+    playerButton.getStyleClass().add("icon-only-button");
+    playerButton.setOnMouseClicked(mouseEvent -> showPlayerColorPickerPopup(mouseEvent.getScreenX(), mouseEvent.getScreenY()));
+
     nameTextField = new TextField(defaultName);
     deleteButton = new Button();
     deleteButton.setGraphic(new FontIcon("fas-trash"));
-    deleteButton.getStyleClass().add("main-menu-delete-player-button");
+    deleteButton.getStyleClass().add("icon-only-button");
 
-    this.getChildren().addAll(playerCircle, nameTextField);
+    this.getChildren().addAll(playerButton, nameTextField);
 
     if (removable) {
       this.getChildren().add(deleteButton);
     }
+  }
+
+  private void showPlayerColorPickerPopup(double xPos, double yPos) {
+    Text popupText = new Text("Pick a color for " + getName());
+    ColorPicker colorPicker = new ColorPicker(getColor());
+    colorPicker.setOnAction(event ->
+        playerButtonCircle.setStroke(colorPicker.getValue()));
+
+    VBox popupContent = new VBox(popupText, colorPicker);
+    popupContent.getStyleClass().add("main-menu-player-color-picker-popup");
+
+    Popup popup = new Popup();
+    popup.getContent().setAll(popupContent);
+    popup.setHideOnEscape(true);
+    popup.setAutoFix(true);
+    popup.setAutoHide(true);
+    popup.show(this.getScene().getWindow(), xPos, yPos);
   }
 
   public void setRunnable(Runnable deleteRowAction) {
@@ -43,5 +71,9 @@ public class MainMenuPlayerRow extends HBox {
 
   public String getName() {
     return nameTextField.getText();
+  }
+
+  public Color getColor() {
+    return (Color) playerButtonCircle.getStroke();
   }
 }
