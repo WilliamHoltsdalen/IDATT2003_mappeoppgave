6d9dac92002b233bef87e1f3e3749b44a1c3524c
WilliamHoltsdalen <williamlh@hotmail.no>
2025-04-03 21:14:17 +0200

    feat(observer): Add onTileActionPerformed() to BoardGameObserver

--- src/main/java/edu/ntnu/idi/idatt/observer/BoardGameObserver.java
+++ src/main/java/edu/ntnu/idi/idatt/observer/BoardGameObserver.java
@@ -1,18 +1,20 @@
 package edu.ntnu.idi.idatt.observer;
 
 import edu.ntnu.idi.idatt.model.Player;
+import edu.ntnu.idi.idatt.model.interfaces.TileAction;
 
 /**
- * An interface for observers that wish to receive notifications about board game events.
+ * An interface for observers that wish to be notified about board game events.
  */
 public interface BoardGameObserver {
   /**
    * Called when a player moves to a new tile.
    *
    * @param player The player who moved.
+   * @param diceRoll The value of the dice roll the player made.
    * @param newTileId The ID of the new tile.
    */
-  void onPlayerMoved(Player player, int newTileId);
+  void onPlayerMoved(Player player, int diceRoll, int newTileId);
 
   /**
    * Called when the game state changes (e.g., round number increments, special events occur).
@@ -20,6 +22,13 @@ public interface BoardGameObserver {
    * @param stateUpdate A description of the state change.
    */
   void onGameStateChanged(String stateUpdate);
+
+  /**
+   * Called when a tile action, like a ladderAction, has been activated/performed.
+   *
+   * @param tileAction the tile action that was performed.
+   */
+  void onTileActionPerformed(TileAction tileAction);
 
   /**
    * Called when the game has finished.
