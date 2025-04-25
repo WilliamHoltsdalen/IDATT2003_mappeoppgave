1dafb5c5d068f3ebef3beac9d26aada91707a4d0
WilliamHoltsdalen <williamlh@hotmail.no>
2025-04-25 20:54:41 +0200

    feat(observer): Add observer interfaces for button clicks

---
+++ src/main/java/edu/ntnu/idi/idatt/observer/ButtonClickObserver.java
@@ -0,0 +1,27 @@
+package edu.ntnu.idi.idatt.observer;
+
+import java.util.Map;
+
+/**
+ * <h3>ButtonClickObserver interface</h3>
+ *
+ * <p>This interface defines methods for observing button click events, such as when a button is
+ * clicked in a view.
+ */
+public interface ButtonClickObserver {
+
+  /**
+   * Handles button click events.
+   *
+   * @param buttonId the ID of the button clicked
+   */
+  void onButtonClicked(String buttonId);
+
+  /**
+   * Handles button click events with parameters.
+   *
+   * @param buttonId the ID of the button clicked
+   * @param params the parameters of the button click event
+   */
+  void onButtonClickedWithParams(String buttonId, Map<String, Object> params);
+}
